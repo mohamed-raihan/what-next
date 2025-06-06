8ff59d8d91be0f6f0f848706a61ca7fd5206cf21
mohamed-raihan <141607655+mohamed-raihan@users.noreply.github.com>
2025-06-06 15:58:47 +0530

    build-6

--- app/blogs/[slug]/page.tsx
+++ app/blogs/[slug]/page.tsx
@@ -1,16 +1,18 @@
-import { blogData } from '../../sections/Blogs/blogData';
-import BlogSection from '../../sections/Blogs/BlogSection';
-import { notFound } from 'next/navigation';
+// import { blogData } from '../../sections/Blogs/blogData';
+// import BlogSection from '../../sections/Blogs/BlogSection';
+// import { notFound } from 'next/navigation';
 
-type PageProps = {
-  params: { slug: string };
-  searchParams: { [key: string]: string | string[] | undefined };
+// type PageProps = {
+//   params: { slug: string };
+//   searchParams: { [key: string]: string | string[] | undefined };
+// }
+
+// export default async function BlogPage({ params }: PageProps) {
+//   const data = blogData[params.slug.toLowerCase()];
+//   if (!data) return notFound();
+//   return <BlogSection {...data} />;
+// } 
+
+export default function BlogPage() {
+    return <div>BlogPage</div>;
 }
-
-export default function BlogPage({ params }: PageProps) {
-    console.log(params.slug);
-    console.log(blogData);
-  const data = blogData[params.slug.toLowerCase()];
-  if (!data) return notFound();
-  return <BlogSection {...data} />;
-} 
