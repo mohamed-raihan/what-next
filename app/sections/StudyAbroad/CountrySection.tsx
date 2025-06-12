350c646afb93dd7c068e3df2ad73cc81c2da6b74
mohamed-raihan <141607655+mohamed-raihan@users.noreply.github.com>
2025-06-12 13:24:07 +0530

    responsive

--- app/sections/StudyAbroad/CountrySection.tsx
+++ app/sections/StudyAbroad/CountrySection.tsx
@@ -14,7 +14,7 @@ const CountrySection: React.FC<CountryData> = ({
     whyChooseCards,
     blueAnimal,
 }) => {
-    
+
     return (
         <div className="w-full">
             {/* Hero Section */}
@@ -99,9 +99,9 @@ const CountrySection: React.FC<CountryData> = ({
             </div>
 
             {/* Contact Section */}
-            <div className="relative bg-[url(/service-bg.svg)] bg-cover flex flex-col xl:flex-row justify-between items-center xl:items-start bg-no-repeat md:-mt-20 pt-10 md:pt-35 px-2 md:px-10 py-10 lg:px-25 h-auto md:h-[80vh]">
+            <div className="relative bg-[url(/service-bg.svg)] bg-cover flex flex-col xl:flex-row justify-between items-center xl:items-start bg-no-repeat md:mt-0 pt-10 md:pt-35 px-2 md:px-10 py-10 lg:px-25 h-auto md:h-[80vh]">
                 <div className="ms-2 md:ms-10 mb-8 md:mb-0 flex-1 flex flex-col">
-                    <h1 className="text-3xl md:text-6xl lg:text-8xl text-[#0046AA] font-medium font-roboto font-semibold">
+                    <h1 className="text-3xl md:text-5xl xl:text-7xl text-[#0046AA] font-medium font-roboto font-semibold">
                         Get in Touch with<br />
                         <span className="text-[#288737]">Our Team</span>
                     </h1>
