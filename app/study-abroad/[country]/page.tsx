8974861055a05c92959f1d9db9509d82a15189be
mohamed-raihan <141607655+mohamed-raihan@users.noreply.github.com>
2025-06-23 19:51:50 +0530

    build2

--- app/study-abroad/[country]/page.tsx
+++ app/study-abroad/[country]/page.tsx
@@ -2,8 +2,12 @@ import { getCountryData } from '../../sections/StudyAbroad/countryData';
 import CountrySection from '../../sections/StudyAbroad/CountrySection';
 import { notFound } from 'next/navigation';
 
-export default async function StudyAbroadCountryPage({ params }: { params: { country: string } }) {
-  const { country } = params;
+interface Props {
+  params: Promise<{ country: string }>;
+}
+
+export default async function StudyAbroadCountryPage({ params }: Props) {
+  const { country } = await params;
   const allCountryData = await getCountryData();
   const data = allCountryData[country.toLowerCase()];
   if (!data) return notFound();
